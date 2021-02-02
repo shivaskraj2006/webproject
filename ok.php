72d8f0ca66e4eeaca484085fbd9f1fdf607cbaad
Shiva Raj Acharya <77542309+shivaskraj2006@users.noreply.github.com>
2021-02-03 01:39:15 +0200

    created new file

--- ok.php
+++ ok.php
@@ -1,4 +1,21 @@
 <?php
 
 echo " his this is me shiva";
- ?>
+
+
+?>
+<br>
+
+<?php
+        /* echo " lets assume two numbers i and j"; */
+
+        for($i=8; $i>0; $i--){
+            for($j=0; $j<$i; $j++){
+                echo $j+1 . " ";
+            }
+            echo "<br />";
+
+        }
+
+        ?>
+
